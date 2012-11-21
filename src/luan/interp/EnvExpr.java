37c64e8926c15179986062e69c5dde462801b245
fschmidt@gmail.com <fschmidt@gmail.com@21e917c8-12df-6dd8-5cb6-c86387c605b9>
2012-11-21 09:40:33 +0000

    add variables and tables

--- src/luan/interp/EnvExpr.java
+++ src/luan/interp/EnvExpr.java
@@ -4,6 +4,9 @@ import luan.LuaState;
 
 
 final class EnvExpr extends Expr {
+	static final EnvExpr INSTANCE = new EnvExpr();
+
+	private EnvExpr() {}
 
 	@Override Object eval(LuaState lua) {
 		return lua.env();
