7cdfdada93bae9f5db9bead40146dbaab689eb0a
fschmidt@gmail.com <fschmidt@gmail.com@21e917c8-12df-6dd8-5cb6-c86387c605b9>
2014-06-16 10:37:06 +0000

    conditions must be type boolean

--- trunk/src/luan/interp/IfStmt.java
+++ trunk/src/luan/interp/IfStmt.java
@@ -2,21 +2,23 @@ package luan.interp;
 
 import luan.Luan;
 import luan.LuanException;
+import luan.LuanSource;
 
 
-final class IfStmt implements Stmt {
+final class IfStmt extends CodeImpl implements Stmt {
 	private final Expr cnd;
 	final Stmt thenStmt;
 	final Stmt elseStmt;
 
-	IfStmt(Expr cnd,Stmt thenStmt,Stmt elseStmt) {
+	IfStmt(LuanSource.Element se,Expr cnd,Stmt thenStmt,Stmt elseStmt) {
+		super(se);
 		this.cnd = cnd;
 		this.thenStmt = thenStmt;
 		this.elseStmt = elseStmt;
 	}
 
 	@Override public void eval(LuanStateImpl luan) throws LuanException {
-		if( Luan.toBoolean( cnd.eval(luan) ) ) {
+		if( luan.bit(se).checkBoolean( cnd.eval(luan) ) ) {
 			thenStmt.eval(luan);
 		} else {
 			elseStmt.eval(luan);
--- trunk/src/luan/interp/WhileStmt.java
+++ trunk/src/luan/interp/WhileStmt.java
@@ -2,20 +2,22 @@ package luan.interp;
 
 import luan.Luan;
 import luan.LuanException;
+import luan.LuanSource;
 
 
-final class WhileStmt implements Stmt {
+final class WhileStmt extends CodeImpl implements Stmt {
 	private final Expr cnd;
 	private final Stmt doStmt;
 
-	WhileStmt(Expr cnd,Stmt doStmt) {
+	WhileStmt(LuanSource.Element se,Expr cnd,Stmt doStmt) {
+		super(se);
 		this.cnd = cnd;
 		this.doStmt = doStmt;
 	}
 
 	@Override public void eval(LuanStateImpl luan) throws LuanException {
 		try {
-			while( Luan.toBoolean( cnd.eval(luan) ) ) {
+			while( luan.bit(se).checkBoolean( cnd.eval(luan) ) ) {
 				doStmt.eval(luan);
 			}
 		} catch(BreakException e) {}
