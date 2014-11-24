c9c5f1d360d1034e25a3c2fb2c90511bb2e47dfc
fschmidt@gmail.com <fschmidt@gmail.com@21e917c8-12df-6dd8-5cb6-c86387c605b9>
2014-11-24 22:22:01 +0000

    log web exceptions

--- trunk/web/src/luan/modules/web/LuanHandler.java
+++ trunk/web/src/luan/modules/web/LuanHandler.java
@@ -5,6 +5,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 import luan.LuanState;
@@ -12,6 +14,7 @@ import luan.LuanException;
 
 
 public class LuanHandler extends AbstractHandler {
+	private static final Logger logger = LoggerFactory.getLogger(LuanHandler.class);
 	private final LuanState luan;
 	private String welcomeFile = "index.html";
 
@@ -29,8 +32,9 @@ public class LuanHandler extends AbstractHandler {
 				return;
 			response.setStatus(HttpServletResponse.SC_OK);
 		} catch(LuanException e) {
-//e.printStackTrace();
-			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,e.getFullMessage());
+			String err = e.getFullMessage();
+			logger.error(err);
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,err);
 		}
 		baseRequest.setHandled(true);
 	}
