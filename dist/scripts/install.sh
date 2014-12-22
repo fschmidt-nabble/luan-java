0ccfd7c571a4e4fd316bf3415d07b4f1f54b7902
fschmidt@gmail.com <fschmidt@gmail.com@21e917c8-12df-6dd8-5cb6-c86387c605b9>
2014-12-22 08:27:08 +0000

    add tar-luan.sh

---
+++ dist/scripts/install.sh
@@ -0,0 +1,11 @@
+cd `dirname $0`
+
+cat >/usr/local/bin/luan <<End
+for i in `pwd`/jars/* ; do CLASSPATH=\$CLASSPATH:\$i ; done
+
+java -classpath \$CLASSPATH luan.Luan \$*
+End
+
+chmod +x /usr/local/bin/luan
+
+echo "the command 'luan' has been installed"
